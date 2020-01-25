2ec4197a4d2e9d7892440b2c07b4bbc1176b81d3
Drew Carrillo <drewc3@gmail.com>
2020-01-25 13:02:59 -0700

    stamps-router

--- src/stamps/stamps-router.js
+++ src/stamps/stamps-router.js
@@ -4,7 +4,7 @@ const xss = require('xss')
 const StampsService = require('./stamps-service')
 const stampsRouter = express.Router()
 const jsonParser = express.json()
-const {requireAuth} = require('../middleware/jwt-auth.js')
+const { requireAuth } = require('../middleware/jwt-auth.js')
 
 const sanatizeStamp = (stamp) => ({
   id: stamp.id,
@@ -17,23 +17,20 @@ const sanatizeStamp = (stamp) => ({
   write: stamp.write
 })
 
-
-
-stampsRouter 
- .use(requireAuth)
+stampsRouter
+  .use(requireAuth)
   .route('/')
   .get((req, res, next) => {
     StampsService.getAllStamps(req.app.get('db'), req.user.id)
-    .then((stamp)=> {
-      res.json(stamp.map(sanatizeStamp))
-    })
-    .catch(next)
-  }) 
+      .then((stamp) => {
+        res.json(stamp.map(sanatizeStamp))
+      })
+      .catch(next)
+  })
   .post(jsonParser, (req, res, next) => {
     const { title, template_id, owner_id, profile_id, content } = req.body
     const newStamp = { title, template_id, owner_id, profile_id, content }
-  
-  
+
     for (const [key, value] of Object.entries(newStamp)) {
       if (value == null) {
         return res.status(400).json({
@@ -43,22 +40,21 @@ stampsRouter
     }
 
     StampsService.insertStamp(req.app.get('db'), newStamp)
-    .then((stamp) => {
-      res
-        .status(201)
-        .location(path.posix.join(req.originalUrl, `/${stamp.id}`))
-        .json(sanatizeStamp(stamp))
-    })
-    .catch(next)
-})
+      .then((stamp) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${stamp.id}`))
+          .json(sanatizeStamp(stamp))
+      })
+      .catch(next)
+  })
 
 stampsRouter
-   .use(requireAuth)
+  .use(requireAuth)
   .route('/:stamp_id')
   // .all(requireAuth)
   .all(checkStampExists)
   .get((req, res, next) => {
-    
     res.json(sanatizeStamp(res.stamp))
   })
   .delete((req, res, next) => {
@@ -72,8 +68,7 @@ stampsRouter
   .patch(jsonParser, (req, res, next) => {
     const { title, profile_id, content } = req.body
     const stampToUpdate = { title, profile_id, content }
-    const numberOfValues = Object.values(stampToUpdate).filter(Boolean)
-      .length
+    const numberOfValues = Object.values(stampToUpdate).filter(Boolean).length
     if (numberOfValues === 0) {
       return res.status(400).json({
         error: {
@@ -84,7 +79,7 @@ stampsRouter
     StampsService.updateStamp(
       req.app.get('db'),
       req.params.stamp_id,
-      stampToUpdate 
+      stampToUpdate
     )
       .then((numRowsAffected) => {
         res.status(204).end()
@@ -92,23 +87,23 @@ stampsRouter
       .catch(next)
   })
 
-  async function checkStampExists(req, res, next) {
-    try { 
-      const stamp = await StampsService.getById(
-        req.app.get('db'),
-        req.params.stamp_id
-      )
-  
-      if (!stamp)
-        return res.status(404).json({
-          error: `Stamp doesn't exist`
-        })
-  
-      res.stamp = stamp
-      next()
-    } catch (error) {
-      next(error)
-    }
+async function checkStampExists(req, res, next) {
+  try {
+    const stamp = await StampsService.getById(
+      req.app.get('db'),
+      req.params.stamp_id
+    )
+
+    if (!stamp)
+      return res.status(404).json({
+        error: `Stamp doesn't exist`
+      })
+
+    res.stamp = stamp
+    next()
+  } catch (error) {
+    next(error)
   }
+}
 
 module.exports = stampsRouter
