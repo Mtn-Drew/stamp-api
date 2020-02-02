4e11456c758d5e4417df5cdd6c8e6fd7804c9cd8
Drew Carrillo <drewc3@gmail.com>
2020-02-02 14:21:58 -0700

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgresql@localhost/stamp',
-  JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-s3cret',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://postgresql@localhost/stamp',
+  JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '24h',
 }
