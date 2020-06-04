1f25658d2ebaaecd776dd28d15c00f6af866c790
Drew Carrillo <drewc3@gmail.com>
2020-06-04 07:05:14 -0600

    pre-deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgresql@localhost/stamp',
   JWT_SECRET: process.env.JWT_SECRET || 'test-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '30d',
